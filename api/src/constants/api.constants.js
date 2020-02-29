072fb2ae33a177d4ceef8a21029566bea57aa378
Roshan Maharjan <roshan.bodom@gmail.com>
2020-02-29 22:53:08 +0545

    matrix

---
+++ api/src/constants/api.constants.js
@@ -0,0 +1,9 @@
+const TMDB_APIKEY = 'd7938fecb2bd0784f7732844628cc9b9';
+const TMDB = `https://api.themoviedb.org/3/search/movie?api_key=${TMDB_APIKEY}`;
+const TMDB_IMAGE_BASE = 'http://image.tmdb.org/t/p/w300';
+
+export {
+  TMDB_APIKEY,
+  TMDB,
+  TMDB_IMAGE_BASE
+};
