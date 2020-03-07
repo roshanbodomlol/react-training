0e7b188d8e2b4a173602abc6f74a75173ca8b66c
Roshan Maharjan <roshan.bodom@gmail.com>
2020-03-08 01:01:38 +0545

    whats new setup

--- spa/src/components/__screens/WhatsNew/withWhatsNew.js
+++ spa/src/components/__screens/WhatsNew/withWhatsNew.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
 
+import { API } from '../../../constants';
+import { get } from '../../../services/api.services';
+
 const withWhatsNew = (WhatsNewComponent) => {
   class WithWhatsNew extends Component {
-    state = {};
+    state = {
+      posts: [],
+      loading: true
+    };
+
+    componentDidMount() {
+      get(API.ENDPOINTS.POSTS_ALL)
+        .then((responsePosts) => {
+          const posts = responsePosts.map((p) => ({ img: p.fimg_url, title: p.title.rendered }));
+          this.setState({ posts, loading: false });
+        })
+        .catch((e) => console.log(e));
+    }
 
     render() {
+      const {
+        posts,
+        loading
+      } = this.state;
+
       return (
-        <WhatsNewComponent/>
+        <WhatsNewComponent
+          posts={posts}
+          loading={loading}
+        />
       );
     }
   }
--- spa/src/services/api.services.js
+++ spa/src/services/api.services.js
@@ -1,8 +1,17 @@
 import axios from 'axios';
 
+import { API } from '../constants';
+
 export const get = (URL) => new Promise((resolve, reject) => {
   axios
-    .get(URL)
+    .get(`${API.API_URL}${URL}`)
+    .then((response) => resolve(response.data))
+    .catch((e) => reject(e));
+});
+
+export const getWoo = (URL) => new Promise((resolve, reject) => {
+  axios
+    .get(`${API.API_WOO_URL}${URL}`)
     .then((response) => resolve(response.data))
     .catch((e) => reject(e));
 });
