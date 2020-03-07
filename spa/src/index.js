c644c4b2488994dd451fd9544533193fcdac335b
Roshan Maharjan <roshan.bodom@gmail.com>
2020-03-07 21:04:54 +0545

    header

--- spa/src/index.js
+++ spa/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'antd/dist/antd.css'
+import 'antd/dist/antd.css';
+import './styles/global.scss';
 
 ReactDOM.render(
   <App/>,
