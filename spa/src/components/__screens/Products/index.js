b7b095d0db1e4c4bf8e59c4aa35bc51b21686ffa
Roshan Maharjan <roshan.bodom@gmail.com>
2020-03-08 01:37:26 +0545

    done

--- spa/src/components/__screens/Products/index.js
+++ spa/src/components/__screens/Products/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
 
+import { WP } from '../../../constants';
 import './Products.scss';
 
-const Products = () => (
-  <div id="products" className="screen __fullHeight"/>
-);
+const Products = () => {
+  const catList = WP.PRODUCT_CATEGORIES.map((cat) => (
+    <span
+      key={`cat-${cat.id}`}
+      role="button"
+      onClick={() => console.log(cat.id)}
+      tabIndex="-1"
+    >
+      {cat.name}
+    </span>
+  ));
+
+  return (
+    <div id="products" className="screen __fullHeight">
+      <div className="cat-bar">
+        {catList}
+      </div>
+      <div className="product-list"/>
+    </div>
+  );
+};
 
 export default Products;
--- spa/src/constants/index.js
+++ spa/src/constants/index.js
@@ -1,2 +1,3 @@
 export { default as GLOBALS } from './global.constants';
 export { default as API } from './api.constants';
+export { default as WP } from './wp.constants';
