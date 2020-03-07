76417874fdc2a5da1be77eb7d4de6f5e7956a4ef
Roshan Maharjan <roshan.bodom@gmail.com>
2020-03-08 01:18:52 +0545

    last

--- spa/src/constants/api.constants.js
+++ spa/src/constants/api.constants.js
@@ -1,8 +1,8 @@
 export default {
   WOO_KEY: 'ck_3c6cd285f72c2d504c163f2be844bd2dd3927ffc',
   WOO_SECRET: 'cs_b3ef5de67dfb120c98f34f437c28c4ac205d74ce',
-  API_URL: 'http://localhost/estore/wp-json/wp/v2',
-  API_WOO_URL: 'http://localhost/estore/wp-json/wc/v2',
+  API_URL: 'http://demos.wearemanda.com/estore/wp-json/wp/v2',
+  API_WOO_URL: 'http://demos.wearemanda.com/estore/wp-json/wc/v2',
   ENDPOINTS: {
     POSTS_ALL: '/posts',
     PRODUCTS_ALL: '/products',
